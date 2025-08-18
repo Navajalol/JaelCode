246816bb1e4cad6dbdff1d31595a224a587a5127
navajalol <navarreteandres112@gmail.com>
2025-08-18 14:40:53 -0400

    Unscaled code

--- Main.py
+++ Main.py
@@ -1,13 +1,29 @@
 import os
+import shutil
+import tkinter as tk
+from tkinter import filedialog
 
 def Main():
-    print("hello World")
-    osScript()
+    root = tk.Tk()
+    root.withdraw()
+    source_folder_path = filedialog.askdirectory(title = "Select a folder")
 
-def osScript():
-    userFile = input()
-    file = open(userFile, 'r')
-    print(file.read())
+    if not source_folder_path:
+        exit()
+
+    dest_folder_path = filedialog.askdirectory(title = "select destination folder")
+    if not dest_folder_path:
+        exit()
+
+    os.makedirs(dest_folder_path, exist_ok=True)
+
+    for file_name in os.listdir(source_folder_path):
+        if file_name.lower().endswith(".mp3"):
+            src_path = os.path.join(source_folder_path, file_name)
+            dst_path = os.path.join(dest_folder_path, file_name) 
+
+    shutil.copy2(src_path, dst_path)
+    print(f"Moved: {file_name}") 
     
 Main()
 
